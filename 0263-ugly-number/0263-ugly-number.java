edc87fe50f1672d2efda84ae907acab6f01d3e0d
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-09-06 22:02:10 +0530

    Time: 0 ms (100%), Space: 40.3 MB (95.63%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,15 +1,34 @@
 class Solution {
     public boolean isUgly(int n) {
-        if (n == 0)
-        return false;
-        int[] x={2,3 ,5};
-        
-        for (int prime : x){
-            while (n % prime == 0){
-                n /= prime;
-            }
+        if (n == 0) {
+            return false;
         }
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                continue;
+            }
+            if (n % 3 == 0) {
+                n = n / 3;
+                continue;
+            }
+            if (n % 5 == 0) {
+                n = n / 5;
+                continue;
+            }
+            return false;
+        }
+        return true;
+        // if (n == 0)
+        // return false;
+        // int[] x={2,3 ,5};
+        
+        // for (int prime : x){
+        //     while (n % prime == 0){
+        //         n /= prime;
+        //     }
+        // }
 
-        return n == 1;
+        // return n == 1;
     }
 }
