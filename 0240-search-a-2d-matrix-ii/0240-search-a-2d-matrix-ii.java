a1d925d5999582e88de79f8ede44d8ec11a3347e
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-09-07 16:50:45 +0530

    Time: 5 ms (99.87%), Space: 45.5 MB (90.52%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,31 +1,42 @@
 class Solution {
-    public int bs(int[] arr,int target){
-        int low=0;
-        int high=arr.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return -1;
-
-    }    
+    // public int bs(int[] arr,int target){
+    //     int low=0;
+    //     int high=arr.length-1;
+    //     while(low<=high){
+    //         int mid=(low+high)/2;
+    //         if(arr[mid]==target){
+    //             return mid;
+    //         }
+    //         else if(arr[mid]<target){
+    //             low=mid+1;
+    //         }
+    //         else{
+    //             high=mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }    
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            int idx=bs(matrix[i], target);
-            if(idx!=-1){
-                return true;
-            }
-            
+    //     for(int i=0;i<matrix.length;i++){
+    //         int idx=bs(matrix[i], target);
+    //         if(idx!=-1){
+    //             return true;
+    //         }
+    //     }
+    int n=matrix.length;
+    int m=matrix[0].length;
+    int row=0; int col=m-1;
+    while(row<n && col>=0){
+        if(matrix[row][col]==target){
+            return true;
         }
+        else if(matrix[row][col]<target){
+            row++;
+        }
+        else{
+            col--;
+        }
+    }
         return false;
-        
     }
 }
