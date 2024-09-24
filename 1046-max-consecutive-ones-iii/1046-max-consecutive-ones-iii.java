8db530947d8ef598047e61bb5d5123d38e38ac19
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-09-24 21:04:54 +0530

    Time: 3 ms (86.53%), Space: 47.1 MB (47.52%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,5 +1,28 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+    //     int maxlen=0;
+        
+    //     for(int i=0;i<nums.length;i++){
+    //         int zeros=0;
+    //         for(int j=i;j<nums.length;j++){
+               
+    //             if(nums[j]==0){
+    //                 zeros++;
+    //             }
+    //             if(zeros<=k){
+    //                 int len=j-i+1;
+    //                 maxlen=Math.max(len,maxlen);
+    //             }
+    //             else{
+    //                 break;
+    //             }
+    //         }
+           
+    //     }
+    //     return maxlen;
+
+    //     }
+    // }
         int l=0;
         int r=0;
         int zeros=0;
@@ -22,3 +45,4 @@ class Solution {
        return maxlen; 
     }
 }
+
