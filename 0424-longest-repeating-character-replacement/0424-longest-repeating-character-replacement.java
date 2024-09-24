df64f6187510702396e4d3848dcfb04a1a41f7c6
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-09-24 21:40:12 +0530

    Time: 7 ms (82.39%), Space: 42.5 MB (63.53%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,48 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-       int maxlen=0;
-       for(int i=0;i<s.length();i++){
-        int hash[]=new int[26];
-        Arrays.fill(hash,0);
+    //    int maxlen=0;
+    //    for(int i=0;i<s.length();i++){
+    //     int hash[]=new int[26];
+    //     Arrays.fill(hash,0);
+    //     int maxf=0;
+    //     for(int j=i;j<s.length();j++){
+    //         hash[s.charAt(j)-'A']++;
+    //         maxf=Math.max(maxf,hash[s.charAt(j)-'A']);
+    //         int changes=(j-i+1)-maxf;
+    //         if(changes<=k){
+    //             int len=j-i+1;
+    //             maxlen=Math.max(len,maxlen);
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //    }
+    //    return maxlen;
+
+        int l=0;
+        int r=0;
+        int maxlen=0;
         int maxf=0;
-        for(int j=i;j<s.length();j++){
-            hash[s.charAt(j)-'A']++;
-            maxf=Math.max(maxf,hash[s.charAt(j)-'A']);
-            int changes=(j-i+1)-maxf;
-            if(changes<=k){
-                int len=j-i+1;
-                maxlen=Math.max(len,maxlen);
+        int hash[]=new int[26];
+        Arrays.fill(hash, 0);
+        while(r<s.length()){
+            hash[s.charAt(r)-'A']++;
+            maxf=Math.max(maxf,hash[s.charAt(r)-'A']);
+            while(((r-l+1)-maxf)>k){
+                hash[s.charAt(l)-'A']--;
+                // maxf=0;
+                // for(int i=0;i<26;i++){
+                //     maxf=Math.max(maxf,hash[s.charAt(r)-'A']);
+                //     l=l+1;
+                // }
+                l++;
             }
-            else{
-                break;
+            if((r-l+1)-maxf<=k){
+                maxlen=Math.max(maxlen,r-l+1);
             }
+            r++;
         }
-       }
-       return maxlen;
+        return maxlen;
     }
 }
