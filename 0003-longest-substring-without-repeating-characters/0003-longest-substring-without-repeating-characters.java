837ac5ecacc39163b4ec9908e4bff09fea4622f4
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-10-05 22:42:42 +0530

    Time: 3 ms (90.96%), Space: 43.6 MB (86.6%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -22,8 +22,7 @@ class Solution {
     int l=0;
     int[] hash = new int[256];
     Arrays.fill(hash, -1);
-    while(r<s.length()){
-        
+    while(r<s.length()){ 
         if(hash[s.charAt(r)]!=-1){
             if(hash[s.charAt(r)]>=l){
                 l=hash[s.charAt(r)]+1;
